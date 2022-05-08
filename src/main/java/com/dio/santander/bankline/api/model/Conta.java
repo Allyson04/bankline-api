6194231aea0722b332453eb7e62515bc97f23446
allyson eduardo <allysoneduardo04.com>
2022-05-08 13:54:29 -0300

    feat: mapped entities with JPA

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,19 +1,26 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
-		private Long numero;
-		private Double saldo;
-		
-		public Long getNumero() {
-			return numero;
-		}
-		public void setNumero(Long numero) {
-			this.numero = numero;
-		}
-		public Double getSaldo() {
-			return saldo;
-		}
-		public void setSaldo(Double saldo) {
-			this.saldo = saldo;
-		}
+	@Column(name="conta_numero")
+	private Long numero;
+	
+	@Column(name="conta_saldo")
+	private Double saldo;
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
 }
